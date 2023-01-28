c6263044a7a7472679b9d6c0b9944e77ec0d7af1
KegaPesik1000-7 <122670058+KegaPesik1000-7@users.noreply.github.com>
2023-01-28 14:45:45 +0300

    Add files via upload

---
+++ пайтон.py
@@ -0,0 +1,16 @@
+g = float(input("Введите вещественное число\a"))
+print(g)
+g=int(g)
+print(g)
+p=input("введите число\t")
+print(p)
+print(p)
+f = input("Введите число\o")
+print(f)
+f = float(f)
+print(f)
+u = input("Введите (имя) (фамилия)\p")
+print("Hello,", u)
+
+
+        
